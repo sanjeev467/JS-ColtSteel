35337011e086763eefcb6cc07a8827524a141249
Sanjeev Pandey <sanjeevpandey467@gmail.com>
2024-07-04 10:54:09 +0530

    added oops in jsgit add .

--- Practice-File/prac.js
+++ Practice-File/prac.js
@@ -1,7 +1,11 @@
-const sing = async () => {
-  return "hue hue";
-};
+// const sing = async () => {
+//   return "hue hue";
+// };
 
-sing().then((data) => {
-  console.log("Hey hey", data);
-});
+// sing().then((data) => {
+//   console.log("Hey hey", data);
+// });
+
+const wave = () => {
+  console.log(this);
+};
